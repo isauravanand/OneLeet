30352aaf4165aced2da456cd601eca293d7edd66
Saurav Anand <sauravanand1608@gmail.com>
2025-12-04 19:52:32 +0530

    Make the seperate folder for these

---
+++ Server/src/routes/user/googleAuth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const googleAuthController = require("../../controllers/user/googleAuthController");
+const router = express.Router();
+
+
+const { validate } = require("../../validations/validate");
+const googleAuthValidation = require("../../validations/user/googleAuthValidation");
+
+//Google-Register/Login
+router.post("/google-login",validate(googleAuthValidation), googleAuthController);
+
+module.exports=router;
---
+++ Server/src/routes/user/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const authController = require("../../controllers/user/authController");
+const { validate } = require("../../validations/validate");
+
+const {
+    signupValidation,
+    loginValidation,
+} = require("../../validations/user");
+
+// Register
+router.post("/register", validate(signupValidation), authController.Register);
+
+// Login
+router.post("/login", validate(loginValidation), authController.Login);
+
+//Logout
+router.post("/logout",authController.Logout)
+
+module.exports = router;
