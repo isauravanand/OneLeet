05b8e585d54dacde9be653c8af33d7eb883119f0
Saurav Anand <sauravanand1608@gmail.com>
2025-12-05 03:23:34 +0530

    Added Cookie parser and pdf Route

--- Server/server.js
+++ Server/server.js
@@ -1,26 +1,24 @@
 require('dotenv').config()
 const express = require("express");
 const connectDB = require('./src/config/db');
-const userRoutes = require("./src/routes/userRoutes")
-const googleAuth = require("./src/routes/googleAuth")
+const userRoutes = require("./src/routes/user/userRoutes")
+const googleAuth = require("./src/routes/user/googleAuth")
+const pdfRoutes = require("./src/routes/content/noteRoutes");
 const cors = require("cors");
 const app = express();
+const cookieParser = require("cookie-parser");
 
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
-
-app.post("/test", (req, res) => {
-    console.log("REQ BODY:", req.body);
-    res.json({ received: req.body });
-});
-
+app.use(cookieParser());
 
 //Apis
 app.use("/api/auth",userRoutes);
 app.use("/api/auth", googleAuth);
+app.use("/api/pdf",pdfRoutes);
 
 
 
