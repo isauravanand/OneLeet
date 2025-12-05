40d9d7ebac3367d17d2de73afed6936fac1e6dc4
Saurav Anand <sauravanand1608@gmail.com>
2025-12-05 23:48:47 +0530

    Setup Cors

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,10 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 app.use(cookieParser());
 
 //Apis
